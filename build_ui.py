c81dda9ae348d7cf3d1077750c69eb97279ed061
Joanna Leng <j.leng@leeds.ac.uk>
2020-10-23 17:15:20 +0100

    Neatened the build_ui python scripts. They use both the command line pyuic command giving full paths to the inputs and outputs and with relative paths to the inputs and outputs as well as using the uic function that can work recursively throuhg a directory but for which there is no set output directory so is not suitable for the current project structure.

--- build_ui.py
+++ build_ui.py
@@ -22,6 +22,7 @@ specific language governing permissions and limitations under the License.
 import os
 import argparse
 import pathlib
+from pathlib import Path
 
 def get_args():
     """
@@ -32,8 +33,9 @@ def get_args():
     """
     parser = argparse.ArgumentParser(
         prog='build_ui',
-        epilog="""Script for automating the compilation of Qt 
+        epilog="""Script for automating the compilation of Qt
         user interfaces created with QDesigner""")
+
 
     parser.add_argument(
         "-d",
@@ -48,6 +50,7 @@ def get_args():
 
     return parser.parse_args()
 
+
 def compile_user_interface(ui_file, py_file):
     """
     convert a qdesigner ui file to a python file
@@ -59,8 +62,16 @@ def compile_user_interface(ui_file, py_file):
         Returns:
             None
     """
+
+
+    # to make the pyuic5 command create relative path for the import functions it creates in the
+    # compiled *_ui.py files it needs to be executed in the form source directory and in the form
+    # pyuic5 some_file_view.ui -o ..\views\some_file_view_ui.py
+    # The source and target can be strings as the command is executed as a string.
     command = "pyuic5 {} -o {}"
+    print(command)
     os.system(command.format(ui_file, py_file))
+
 
 def list_ui_files(directory):
     """
@@ -75,9 +86,10 @@ def list_ui_files(directory):
     files = os.listdir(directory)
     return [f for f in files if f.endswith(".ui")]
 
+
 def list_compiled_ui_files(directory):
     """
-    list all compiled ui files (Ui_*) in a directory
+    list all compiled ui files (*_ui.py) in a directory
 
         Args:
             directory (string) the directory
@@ -86,7 +98,7 @@ def list_compiled_ui_files(directory):
             list of files in directory starting with Ui_
     """
     files = os.listdir(directory)
-    return [f for f in files if f.startswith("Ui_")]
+    return [f for f in files if f.endswith("_ui.py")]
 
 def main():
     """
@@ -100,24 +112,56 @@ def main():
         return
     print("Path: {}".format(path))
 
+    path_in = path.joinpath("resources")
+    if not path_in.is_dir():
+        print("Error {} is not a directory".format(path))
+        return
+    print("Resources Path: {}".format(path_in))
+
+
+    path_out = path.joinpath("views")
+    if not path_out.is_dir():
+        print("Error {} is not a directory".format(path))
+        return
+    print("Views Path: {}".format(path_out))
+
+    cwd_start = os.getcwd()
+    print("cwd_start: ", cwd_start)
+    os.chdir("cgt/resources")
+    cwd_resources = os.getcwd()
+    print("cwd_resources: ", cwd_resources)
+    os.chdir(cwd_start)
+    os.chdir("cgt/views")
+    cwd_views = os.getcwd()
+    print("cwd_views: ", cwd_views)
+
     if args.clean:
-        files = list_compiled_ui_files(path)
-        print("{} files to be removed".format(len(files)))
-        for file in files:
-            target = path.joinpath(file)
+        files_to_remove = list_compiled_ui_files(cwd_views)
+        print("{} files to be removed".format(len(files_to_remove)))
+        #files_to_process = list_compiled_ui_files(cwd_resources)
+        #print("{} files to be processed".format(len(files_to_process)))
+        for file in files_to_remove:
+            target = Path(cwd_views).joinpath(file)
             print("\t{}".format(target))
             target.unlink()
 
-    files = list_ui_files(path)
-    print("{} files to be compiled:".format(len(files)))
-    for file in files:
-        source = path.joinpath(file)
+    files_to_process = list_ui_files(cwd_resources)
+    print("{} files to be compiled:".format(len(files_to_process)))
+    view_relative_path = "../views"
+    for file in files_to_process:
+        print("type(file) ", type(file))
+        source = Path(cwd_resources).joinpath(file)
+        print("source: ", source)
         file = file.rsplit('.', 1)[0]
-        file += ".py"
-        file = "Ui_" + file
-        target = path.joinpath(file)
+        file += "_ui.py"
+# #        file = "Ui_" + file
+        print("File: ", file)
+        #target = path_out.joinpath(file)
+        target = view_relative_path+'\\'+file
+        print("target: ", target)
         print("\t{} => {}".format(source, target))
-        compile_user_interface(source, target)
+        compile_user_interface(file, target)
+        #compile_user_interface(source, target)
 
 if __name__ == "__main__":
     main()
--- build_ui2.py
+++ build_ui2.py
@@ -20,9 +20,13 @@ specific language governing permissions and limitations under the License.
 @author: j.h.pickering@leeds.ac.uk
 """
 import os
+from os import remove
+from os.path import join, isfile
 import argparse
 import pathlib
-from pathlib import Path
+from pathlib import Path 
+from PyQt5 import uic
+from PyQt5.uic import compileUiDir
 
 def get_args():
     """
@@ -35,6 +39,12 @@ def get_args():
         prog='build_ui',
         epilog="""Script for automating the compilation of Qt
         user interfaces created with QDesigner""")
+
+#     parser.add_argument(
+#         "-d",
+#         "--directory",
+#         help="Source directory",
+#         required=True, type=str)
     parser.add_argument(
         "-c",
         "--clean",
@@ -43,6 +53,25 @@ def get_args():
 
     return parser.parse_args()
 
+def compile_user_interface(ui_file, py_file):
+    """
+    convert a qdesigner ui file to a python file
+
+        Args:
+            ui_file (pathlib.Path) the full path to the ui file
+            py_file (pathlib.Path) the full path to the python file
+
+        Returns:
+            None
+    """
+
+    # to make the pyuic5 command create relative path for the import functions it creates in the
+    # compiled *_ui.py files it needs to be executed in the form source directory and in the form
+    # pyuic5 some_file_view.ui -o ..\views\some_file_view_ui.py
+    # The source and target can be strings as the command is executed as a string.
+    command = "pyuic5 {} -o {}"
+    print(command)
+    os.system(command.format(ui_file, py_file))
 
 def list_ui_files(directory):
     """
@@ -57,7 +86,6 @@ def list_ui_files(directory):
     files = os.listdir(directory)
     return [f for f in files if f.endswith(".ui")]
 
-
 def list_compiled_ui_files(directory):
     """
     list all compiled ui files (*_ui.py) in a directory
@@ -71,6 +99,16 @@ def list_compiled_ui_files(directory):
     files = os.listdir(directory)
     return [f for f in files if f.endswith("_ui.py")]
 
+def py_name(py_dir, py_file):
+    """
+    Local function to create the Python source file name for the compiled
+    .ui file.
+    
+    @param py_dir suggested name of the directory (string)
+    @param py_file suggested name for the compile source file (string)
+    @return tuple of directory name (string) and source file name (string)
+    """
+    return py_dir, "Ui_{0}".format(py_file)
 
 def main():
     """
@@ -108,16 +146,15 @@ def main():
     print("type(path_out): ", type(path_out))
 
     os.chdir("cgt/resources")
-    cwd_resources = os.getcwd()
-    print("cwd_resources: ", cwd_resources)
+    #cwd_resources = os.getcwd()
+    #print("cwd_resources: ", cwd_resources)
 #     os.chdir(cwd_start)
 #     os.chdir("cgt/views")
 #     cwd_views = os.getcwd()
 #     print("cwd_views: ", cwd_views)
-#
-
+# 
     if args.clean:
-        files_to_remove = list_compiled_ui_files(path_out)
+        files_to_remove = list_compiled_ui_files(path_in)
         print("{} files to be removed".format(len(files_to_remove)))
         #files_to_process = list_compiled_ui_files(cwd_resources)
         #print("{} files to be processed".format(len(files_to_process)))
@@ -126,27 +163,29 @@ def main():
             print("\t{}".format(target))
             target.unlink()
 
-    files_to_process = list_ui_files(cwd_resources)
+    target_dir = "cgt/views"
+    init_fname = "__init__.py"
+    files_to_process = list_ui_files(path_in)
     print("{} files to be compiled:".format(len(files_to_process)))
     view_relative_path = "../views"
     for file in files_to_process:
         print("file: ", file)
         print("type(file) ", type(file))
+        if file != init_fname and isfile(join(target_dir, file)):
+            remove(join(target_dir, file))
 
-        target_file = file.rsplit('.', 1)[0]
-        target_file += "_ui.py"
-        target = view_relative_path+'\\'+target_file
-        print("target: ", target)
+    #compileUiDir(path_in, map=lambda dir, fname: (path_in, fname), from_imports=True)
+    #compileUiDir(path_in, recurse=True, py_name(path_in, "ui_{0}".format(py_file))
+    compileUiDir(path_in, recurse=True)
 
-        # to make the pyuic5 command create relative path for the import functions it creates in the
-        # compiled *_ui.py files it needs to be executed in the form source directory and in the
-        # form pyuic5 some_file_view.ui -o ..\views\some_file_view_ui.py
-        # The source and target can be strings as the command is executed as a string.
-        # On testing this I am sure this does what I had hoped.
-        command = r"pyuic5 "+file+r" -o ..\views\\"+target_file
-        print("command: ", command)
-        os.system(command)
-
+#         target_file = file.rsplit('.', 1)[0]
+#         target_file += "_ui.py" 
+#         target = view_relative_path+'\\'+target_file
+#         print("target: ", target)
+# 
+#         command = r"pyuic5 -d --resource-suffix=cgt.views "+file+r" -o ..\views\\"+target_file
+#         print("command: ", command)
+#         os.system(command)
 
 if __name__ == "__main__":
     main()
