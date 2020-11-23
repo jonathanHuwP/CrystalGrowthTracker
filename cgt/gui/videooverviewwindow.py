c2dfc539f03ad125d79ee207ac303f2a292ed50f
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2020-11-23 11:53:12 +0000

    standardised the declarations

--- cgt/gui/videooverviewwindow.py
+++ cgt/gui/videooverviewwindow.py
@@ -6,22 +6,19 @@ how the image mean value changes over the course of a video.
 This is used an unprocessed (without Euler's Magnifier) video file
 showing dynamic crystallisation of X-ray synchrotron videos.
 
-Joanna Leng (an EPSRC funded Research Software Engineering Fellow (EP/R025819/1)
-University of Leeds
-June 2020
-
-Copyright 2020
-
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this file except in compliance with the License. You may obtain a copy of the
-License at
-http://www.apache.org/licenses/LICENSE-2.0
+License at http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software distributed
 under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 
+This work was funded by Joanna Leng's EPSRC funded RSE Fellowship (EP/R025819/1)
+
+@copyright 2020
+@author: j.h.pickering@leeds.ac.uk and j.leng@leeds.ac.uk
 '''
 
 import sys
--- cgt/io/readcsvreports.py
+++ cgt/io/readcsvreports.py
@@ -4,22 +4,19 @@ readcsvreports.py
 This python module contains functions that create reports in csv
 format for the CrystalGrowthTracker application.
 
-Joanna Leng (an EPSRC funded Research Software Engineering Fellow (EP/R025819/1))
-Jonathan Pickering (also funded on this fellowship)
-University of Leeds
-June 2020
-
-Copyright 2020
-
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this file except in compliance with the License. You may obtain a copy of the
-License at
-http://www.apache.org/licenses/LICENSE-2.0
+License at http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software distributed
 under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
+
+This work was funded by Joanna Leng's EPSRC funded RSE Fellowship (EP/R025819/1)
+
+@copyright 2020
+@author: j.h.pickering@leeds.ac.uk and j.leng@leeds.ac.uk
 '''
 import os
 from pathlib import Path
