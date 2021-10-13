1ba1e46865e0916b7d1cc21529bb385bb6ae95a0
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2021-10-13 18:04:09 +0100

    imporved auto linting

--- auto_pylint.py
+++ auto_pylint.py
@@ -20,6 +20,19 @@ import subprocess
 import pathlib
 import json
 import argparse
+from collections import namedtuple
+
+## data type for results
+PylintIssues = namedtuple("PylintIssues", ["error",
+                                           "warning",
+                                           "refactor",
+                                           "convention"])
+
+def total_issues(issues):
+    """
+    count total numbe of issues in a PylintIssues container
+    """
+    return len(issues.error) + len(issues.warning) + len(issues.refactor) + len(issues.convention)
 
 def get_args():
     """
@@ -32,12 +45,19 @@ def get_args():
                         required=False,
                         help="directory to be searched")
 
-
     parser.add_argument("-f",
                         "--file",
                         type=str,
                         required=False,
                         help="target file")
+
+    parser.add_argument("-t",
+                        "--type",
+                        type=str,
+                        choices = ["error", "warning", "refactor", "convention"],
+                        required=False,
+                        help="Report only type: error, warning, refactor, convention")
+
 
     return parser.parse_args()
 
@@ -80,17 +100,121 @@ def analyse_output(file, results):
             file (pathlib.Path) the file
             results ([dict]) results in JSON format
         Returns:
-            (LintResult)
+            PylintIssues: holding arrays of (line, description) pairs
     """
     num_issues = len(results)
 
     if num_issues == 0:
         print(f"File {file} no problems")
-        return
+        return None
 
-    print(f"File: {file}\n========")
+    return process_pylint_results(results)
+
+def process_pylint_results(results):
+    """
+    process the results of pylint run on a file
+        Args:
+            results [dict]: list of the json results
+        Returns:
+            PylintIssues: holding arrays of (line, description) pairs
+    """
+    error = []
+    warning = []
+    refactor = []
+    convention = []
+
     for issue in results:
-        print(f"\t{issue['line']} => {issue['message']}")
+        if issue["type"] == "convention":
+            convention.append((issue['line'], issue['message']))
+        elif issue["type"] == "refactor":
+            refactor.append((issue['line'], issue['message']))
+        elif issue["type"] == "warning":
+            warning.append((issue['line'], issue['message']))
+        elif issue["type"] == "error":
+            error.append((issue['line'], issue['message']))
+
+    return PylintIssues(error, warning, refactor, convention)
+
+def print_issues(issue_files):
+    """
+    print a lint of all the file and issues
+        Args:
+            issue_files ([(file, PylintIssues)])
+    """
+    if issue_files:
+        for (file, issues) in issue_files:
+            print(f"File {file}: {total_issues(issues)} issues")
+
+            print(f"    {len(issues.error)} Error")
+            for (line, description) in issues.error:
+                print(f"        Line {line}: {description}")
+
+            print(f"    {len(issues.warning)} Warning")
+            for (line, description) in issues.warning:
+                print(f"        Line {line}: {description}")
+
+            print(f"    {len(issues.refactor)} Refactor")
+            for (line, description) in issues.refactor:
+                print(f"        Line {line}: {description}")
+
+            print(f"    {len(issues.convention)} Convention")
+            for (line, description) in issues.convention:
+                print(f"        Line {line}: {description}")
+
+def display_all(linting_list):
+    """
+    print all issues found in files in
+        Args:
+            linting_list ([(pathlib.Path, PylintIssues)])
+    """
+    issue_files = []
+    for file, results in linting_list.items():
+        issues = analyse_output(file, results)
+        if issues  is not None:
+            issue_files.append((file, issues))
+        print_issues(issue_files)
+
+def filter_output(issue_type, results):
+    """
+    filter issues of a give type
+    """
+    if not results:
+        return None
+
+    issues = []
+    for issue in results:
+        if issue["type"] == issue_type:
+            issues.append((issue['line'], issue['message']))
+
+    return issues
+
+def print_issues_type(issue_type, issue_files):
+    """
+    print issues of given type
+        Args:
+            issue_type (string): the type
+            issue_files ([(file, PylintIssues)])
+    """
+    if issue_files:
+        for (file, issues) in issue_files:
+            if issues is not None and issues:
+                print(f"File {file}: {len(issues)} of type {issue_type}")
+                for (line, description) in issues:
+                    print(f"    Line {line}: {description}")
+
+def display_type(issue_type, linting_list):
+    """
+    display issues of specific type
+        Args:
+            issue_type (string): the issue type class
+            linting_list ([(pathlib.Path, PylintIssues)])
+    """
+    issue_files = []
+    for file, results in linting_list.items():
+        issues = filter_output(issue_type, results)
+        issue_files.append((file, issues))
+
+    print_issues_type(issue_type, issue_files)
 
 def main():
     """
@@ -111,8 +235,10 @@ def main():
 
     linting_list = pylint_files(files)
 
-    for file, results in linting_list.items():
-        analyse_output(file, results)
+    if args.type is not None:
+        display_type(args.type, linting_list)
+    else:
+        display_all(linting_list)
 
 if __name__ == "__main__":
     main()
